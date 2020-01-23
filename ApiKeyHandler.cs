142c7a8ca6036a214471c128f313e365eb28a95b
Vladimir Volodin <vlad@devmiles.com>
2020-01-23 19:43:50 +0200

    init

---
+++ ApiKeyHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TocLoaderApp
+{
+    public class ApiKeyHandler : DelegatingHandler
+    {
+        private string ApiKey;
+
+        public ApiKeyHandler(string apiKey) : base()
+        {
+            ApiKey = apiKey;
+        }
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            //request.RequestUri = new Uri(request.RequestUri.AbsoluteUri + "?api_key=n0tAr3alAp1K3y");
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("apikey", ApiKey);
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using CommandLine;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Teamorgchart;
+
+namespace TocLoaderApp
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('u', "uniqueid", Required = false, HelpText = "Unique ID field for a person.", Default = "UniqueId" )]
+            public string UniqueId { get; set; }
+            [Option('i', "input", Required = true, HelpText = "Input file name.")]
+            public string InputFileName { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunProgram);
+        }
+
+        static void RunProgram(Options options)
+        {
+            Console.WriteLine($"Opening file {options.InputFileName}");
+            var rows = ReadRowsFromCsv(options.InputFileName);
+            
+            foreach (IDictionary<String, Object> row in rows)
+            {
+                var uniqueId = (string)row[options.UniqueId];
+                if (string.IsNullOrWhiteSpace(uniqueId))
+                {
+                    Console.WriteLine($"Item does not have a valid Unique Id field in '{options.UniqueId}' column");
+                }
+                else
+                {
+                    Console.WriteLine($"Unique Id is {uniqueId}");
+
+                }
+            }
+            var client = new TeamOrgChartWebUIDirectoryApp(new ApiKeyHandler("4BDa0n.n4nAYJfsxdJT32pj"));
+            var uploaded = client.ChartDataApi.CreateChartItems("891d471e-6d77-41f0-97b0-74868f7e0982", rows.ToList(), "1");
+            Console.WriteLine($"Total number of items created: {uploaded.Count}");
+        }
+
+        static IEnumerable<dynamic> ReadRowsFromCsv(string filePath)
+        {
+            List<dynamic> results = new List<dynamic>();
+            using (TextReader tr = File.OpenText(filePath))
+            {
+                var conf = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    //HasHeaderRecord = false
+                };
+                using (var reader = new CsvReader(tr, conf))
+                {
+                    results.AddRange(reader.GetRecords<dynamic>().ToList());
+                }
+            }
+            return results;
+        }
+    }
+}
